e5d839ffa8396b836b6b00a07539181c2d2a76d1
JianFeng <2834294740@qq.com>
2024-08-17 14:42:00 +0800

    修复wave短线段不显示的问题

--- example/STM32F103C8T6/MultMenu/menu/wave.c
+++ example/STM32F103C8T6/MultMenu/menu/wave.c
@@ -7,13 +7,15 @@ uint8_t buffer[HOR_RES];
 void Wave_Widget(xpMenu Menu)
 {
     uint8_t time;
-    char str[20];
+    char str[30];
     for (time = HOR_RES - 1; time > 0; time--)
     {
         buffer[time] = buffer[time - 1];
     }
     buffer[0] = *(int *)(Menu->now_item->element->data->ptr) % (VER_RES - 20);
     OLED_ClearBuffer();
+    uint8_t color = 1;
+    OLED_SetDrawColor(&color);
     for (time = 0; time < HOR_RES - 1; time++)
     {
         OLED_DrawLine(time, buffer[time], time + 1, buffer[time + 1]);
